9706b455eec01e35acdb9c387fb33d16d5d1a004
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-05-11 20:28:43 +0200

    ok guy

--- the-noise/src/App.js
+++ the-noise/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import HomePage from './components/HomePage'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
--- the-noise/src/components/HomePage.jsx
+++ the-noise/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import './HomePage.css';
 function HomePage () {
     return (
         <div>
-
+            <img src="the-noise/src/images/theatre.jpg" alt=""/>
         </div>
     )
 }
