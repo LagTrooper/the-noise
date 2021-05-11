2ba69f8cd1ed00dd02529650808ca76a2116b280
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-05-11 23:11:32 +0200

    ok ok

--- the-noise/src/components/Navbar.jsx
+++ the-noise/src/components/Navbar.jsx
@@ -8,24 +8,23 @@ function Navbar() {
                 <input type="checkbox" id="show-menu" />
                 <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
                 <div class="content">
-                    <div class="logo"><a href="#">CodingNepal</a></div>
-                        <ul class="links">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Final</a></li>
-                            <li>
-                                <a href="#" class="desktop-link">Candidats</a>
-                                <input type="checkbox" id="show-features" />
-                                <label for="show-features">Candidats</label>
-                                <ul>
-                                    <li><a href="#">Candidats 1</a></li>
-                                    <li><a href="#">Opportunity</a></li>
-                                    <li><a href="#">Perseverance</a></li>
-                                    <li><a href="#">Spirit</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
+                    <div class="logo"><a href="#">Logo</a></div>
+                    <ul class="links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Final</a></li>
+                        <li>
+                            <a href="#" class="desktop-link">Candidats</a>
+                            <input type="checkbox" id="show-features" />
+                            <label for="show-features">Candidats</label>
+                            <ul>
+                                <li><a href="#">Candidats 1</a></li>
+                                <li><a href="#">Opportunity</a></li>
+                                <li><a href="#">Perseverance</a></li>
+                                <li><a href="#">Spirit</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>                  
     )
