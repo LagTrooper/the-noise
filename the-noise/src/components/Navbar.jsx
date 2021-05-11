34504fe85f92118d844fa36b9b608764ad49842d
Kevin <kevin.bouaziz1@gmail.com>
2021-05-12 00:39:15 +0200

    add logo

--- the-noise/src/components/Navbar.jsx
+++ the-noise/src/components/Navbar.jsx
@@ -1,33 +1,50 @@
 import './Navbar.css';
+import logo from '../images/logo.png';
 
 function Navbar() {
-    return (
-        <div class="wrapper">
-            <nav>
-                <input type="checkbox" id="show-search" />
-                <input type="checkbox" id="show-menu" />
-                <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
-                <div class="content">
-                    <div class="logo"><a href="#">Logo</a></div>
-                    <ul class="links">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Final</a></li>
-                        <li>
-                            <a href="#" class="desktop-link">Candidats</a>
-                            <input type="checkbox" id="show-features" />
-                            <label for="show-features">Candidats</label>
-                            <ul>
-                                <li><a href="#">Candidats 1</a></li>
-                                <li><a href="#">Opportunity</a></li>
-                                <li><a href="#">Perseverance</a></li>
-                                <li><a href="#">Spirit</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>                  
-    )
-};
+  return (
+    <div class='wrapper'>
+      <nav>
+        <input type='checkbox' id='show-search' />
+        <input type='checkbox' id='show-menu' />
+        <label for='show-menu' class='menu-icon'>
+          <i class='fas fa-bars'></i>
+        </label>
+        <div class='content'>
+          <img classname='logo' src={logo} alt='' />
+          <ul class='links'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>Final</a>
+            </li>
+            <li>
+              <a href='#' class='desktop-link'>
+                Candidats
+              </a>
+              <input type='checkbox' id='show-features' />
+              <label for='show-features'>Candidats</label>
+              <ul>
+                <li>
+                  <a href='#'>Candidats 1</a>
+                </li>
+                <li>
+                  <a href='#'>Opportunity</a>
+                </li>
+                <li>
+                  <a href='#'>Perseverance</a>
+                </li>
+                <li>
+                  <a href='#'>Spirit</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 export default Navbar;
