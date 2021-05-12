6eaa19e4d8c1d3508fd261e22c8b9f77942bb005
Kevin <kevin.bouaziz1@gmail.com>
2021-05-12 10:15:22 +0200

    modif affichage homepage

--- the-noise/src/App.js
+++ the-noise/src/App.js
@@ -14,7 +14,6 @@ function App() {
     <div className='App'>
       <Router>
         <Navbar />
-      <HomePage />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route
