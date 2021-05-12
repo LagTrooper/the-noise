de6fc2429d7ac475304a5ae465d6a914b1ec6d09
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-05-12 07:41:31 +0200

    bouton homepage fonctionnel

--- the-noise/src/components/HomePage.jsx
+++ the-noise/src/components/HomePage.jsx
@@ -1,8 +1,20 @@
 import './HomePage.css';
+import LinkButton from './LinkButton';
 
-function HomePage () {
+function HomePage() {
+
     return (
-        <article> Hello world</article>
+        <div >
+            <div className="text">
+                <h1>This is the Noise !!</h1>
+                <p>explication text mother fucker</p>
+            </div>
+            <LinkButton to='/thefinal'
+                onClick={(event) => {console.log('custom event here!', event) }}>
+                Vote Now !!
+            </LinkButton>
+
+        </div>
     )
 }
 
