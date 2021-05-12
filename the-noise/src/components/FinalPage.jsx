6425b57a995b9312e4d5ce25996a56827ab27bb5
Ilyas Hara <harailyas@gmail.com>
2021-05-12 11:41:01 +0200

    final page responsive with flex wrap

--- the-noise/src/components/FinalPage.jsx
+++ the-noise/src/components/FinalPage.jsx
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 const ContainerPage = styled.div`
 display: flex;
 justify-content: space-evenly;
+flex-wrap: wrap;
 `
 const MusicCard = styled.div`
 display: flex;
