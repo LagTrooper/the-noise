933f36c6fb67193c11f41b0589f10473a69eb0c9
thomas31toulouse <thomas-jondeau@hotmail.fr>
2021-05-12 10:36:59 +0200

    fixed navbar

--- the-noise/src/components/HomePage.jsx
+++ the-noise/src/components/HomePage.jsx
@@ -6,9 +6,9 @@ function HomePage() {
     return (
         <div>
             <div className="text">
-                <h1 className="title-img">The noise: The final four. </h1>
+                <h1 className="title-img">The Noise: The final! </h1>
                 <img src="https://b2cproductionsite-be80wkthrgjjkmzuip0.netdna-ssl.com/sites/b2c/files/styles/orange_blog_in_picture/public/generated/blog/tvvv-studio-convertimage.jpg?itok=R7zYGq-r" alt="chairs" />
-                <p className="final-text">Who's gonna be the next noise superstar ? </p>
+                <p className="final-text">Who's gonna be the next Noise Superstar ? </p>
             </div>
             <LinkButton to='/thefinal'
                 onClick={(event) => {console.log('custom event here!', event) }}>
